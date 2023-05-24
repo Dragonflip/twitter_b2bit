4873a13457b686bdfdaf6c4d086192f5fbcbd7ed
Dragonflip <jpmarquesdeoliveira@gmail.com>
2023-05-24 17:19:33 -0300

    adicionando posts e authentication

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
