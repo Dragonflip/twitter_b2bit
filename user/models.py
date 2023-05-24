7102bd8732ad248aa036d0d9ddfce2752ebf81c7
Dragonflip <jpmarquesdeoliveira@gmail.com>
2023-05-24 11:44:14 -0300

    adicionando signals para profile picture

--- user/models.py
+++ user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Perfil(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=250)
-    data_nascimento = models.DateField()
+    data_nascimento = models.DateField(null=True)
     profile_picture = models.ImageField(upload_to=None)
+
