2b04a848af8ec08a08b1f8f3558474b071bf1f00
Dragonflip <jpmarquesdeoliveira@gmail.com>
2023-05-28 22:20:30 -0300

    adicionando settings para testes

--- posts/migrations/0002_alter_post_options.py
+++ posts/migrations/0002_alter_post_options.py
@@ -4,14 +4,13 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('posts', '0001_initial'),
+        ("posts", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='post',
-            options={'ordering': ['-created_at']},
+            name="post",
+            options={"ordering": ["-created_at"]},
         ),
     ]
