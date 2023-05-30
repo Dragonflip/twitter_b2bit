e749c4c26f2efd17033e2d50e868fdb4b0be7e41
Dragonflip <jpmarquesdeoliveira@gmail.com>
2023-05-29 21:26:53 -0300

    adicionando s3

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='post_image', null=True)
 
     class Meta:
         ordering = ["-created_at"]
