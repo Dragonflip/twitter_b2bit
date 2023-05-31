34519b1f8e1e4d216386e139ee622e781346c52b
Dragonflip <jpmarquesdeoliveira@gmail.com>
2023-05-31 16:17:04 -0300

    a

--- core/settings.py
+++ core/settings.py
@@ -4,9 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 JWT_SECRET = os.getenv("JWT_SECRET")
